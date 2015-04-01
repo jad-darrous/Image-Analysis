8ecec54e9a5c4bc44fb3ce0cad36d73ad262e4fa
Jad DARROUS <jad.darrous@gmail.com>
2015-04-01 11:58:38 +0200

    finish the TP4, but need a lot more of work

--- TP4/myprogram.c
+++ TP4/myprogram.c
@@ -17,6 +17,9 @@ double G55[25] = {
 	4/GC, 9/GC, 12/GC, 9/GC, 4/GC,
 	2/GC, 4/GC, 5/GC, 4/GC, 2/GC};
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
 int* double_to_int(double *arr, int size) {
 
 	int i;
@@ -109,10 +112,143 @@ void compute_Ix2_Iy2_IxIy(double* Ix, double* Iy, int cols, int rows,
 
 	int i;
 	for (i=0; i < size; i++) {
-		*Ix2[i] = Ix[i] * Ix[i];
-		*Iy2[i] = Iy[i] * Iy[i];
-		*IxIy[i] = Ix[i] * Iy[i];
+		(*Ix2)[i] = Ix[i] * Ix[i];
+		(*Iy2)[i] = Iy[i] * Iy[i];
+		(*IxIy)[i] = Ix[i] * Iy[i];
+		// printf("%d\n", i);
 	}
+}
+
+
+double* harris(int cols, int rows, double* A, double* B, double* C, double a) {
+	int size = cols * rows;
+	double* img = (double *) malloc(size * sizeof(double));
+	int i;
+	for (i=0; i < size; i++) {
+		double detM = A[i]*B[i] - C[i]*C[i];
+		double traceM = A[i]+B[i]; 
+		double H = detM - a * traceM * traceM;
+		img[i] = H;
+	}
+	return img;
+}
+
+int is_local_maxima(double* img, int cols, int i, int j) {
+	int k, l;
+	double val = img[i * cols + j];
+	for (k = -1; k <= 1; ++k)
+		for (l = -1; l <= 1; ++l)
+			if ((k || l) && img[(i+k)*cols + j+l] > val) {
+				return 0;
+			}
+	return 1;
+}
+
+int cmpfunc (const void * a, const void * b)
+{
+   return *(double*)b - *(double*)a;
+}
+
+double getThreshold(double* img, int cols, int rows, int n) {
+	double* maxi = (double *) malloc(cols * rows * sizeof(double));
+	int i, j, c=0;
+	for(i=1; i < rows-1; i++)
+		for(j=1; j < cols-1; j++) {
+			if (is_local_maxima(img, cols, i, j))
+				maxi[c++] = img[i*cols+j];
+	}
+	qsort(maxi, c, sizeof(double), cmpfunc);
+	int val = maxi[n];
+	free (maxi);
+	return val;
+}
+
+
+int* add_red(double* ff, double* img, int cols, int rows, double threshold) {
+	int size = cols * rows;
+	int* res = (int *) malloc(3 * size * sizeof(int));
+	int i;
+	for (i=0; i < size; i++) {
+		if (ff[i] > threshold){
+			res[3*i] = 255;
+			res[3*i+1] = 0;
+			res[3*i+2] = 0;
+		} else {
+			res[3*i] = res[3*i+1] = res[3*i+2] = (int)img[i];
+		}
+	}
+	return res;
+}
+
+int* norm_harris_x(double* img, double* filter, int cols, int rows, int n) {
+	double d = getThreshold(img, cols, rows, n);
+	return add_red(img, filter, cols, rows, d);
+}
+int* norm_harris(double* img, int cols, int rows) {
+/*	double d = getThreshold(img, cols, rows, 20);
+	return add_red(img, cols, rows, d);*/
+	int size = cols * rows;
+	int* res = (int *) malloc(size * sizeof(int));
+	double* maxi = (double *) malloc(size * sizeof(double));
+	int i, j, c=0;
+	for (i=0; i < size; i++) {
+		if (img[i] > 0.3)
+			res[i] = 255;
+		else if (img[i] < -0.3)
+			res[i] = 125;
+		else
+			res[i] = 0;
+		// res[i] = (int)img[i];
+	}
+	for (i=0; i < size; i++) res[i] = 0;
+	for(i=1; i < rows-1; i++)
+		for(j=1; j < cols-1; j++) {
+			// img[i*cols+j] = eign[i*cols+j] > 50 ? 255 : 0;
+			// res[i*cols+j] = is_local_maxima(img, cols, i, j) ? 255 : 0;
+			if (is_local_maxima(img, cols, i, j))
+				maxi[c++] = img[i*cols+j];
+			// printf("%0.2f\t", img[i]);
+	}
+	int n = 20;
+	qsort(maxi, c, sizeof(double), cmpfunc);
+	for (i=0; i < size; i++) 
+		res[i] = img[i] > maxi[n] ? 255 : 0;
+	return res;
+}
+
+#define tolerance 0.1e-20
+
+void eigenvalues(double A, double B, double C, double D, double* lambda1, double* lambda2) {
+	if(B*C <= tolerance)  {
+		*lambda1 = A; // *v1x  =  1;  *v1y  =  0;
+		*lambda2 = D; // *v2x  =  0;  *v2y  =  1;
+		return;
+	}
+	double tr =  A  +  D;
+	double det =  A  *  D  -  B  *  C;
+	double S = sqrt(pow(tr/2, 2) - det);
+	*lambda1 = tr/2 + S;
+	*lambda2 = tr/2 - S;
+}
+
+int* ShiTomasi(int cols, int rows, double* A, double* B, double* C) {
+	int size = cols * rows;
+	double lambda1, lambda2;
+	double* eign = (double *) malloc(size * sizeof(double));
+	int i, j;
+	for (i=0; i < size; i++) {
+		eigenvalues(A[i], C[i], C[i], B[i], &lambda1, &lambda2);
+		eign[i] = MIN(lambda1, lambda2);
+	}
+	int* img = (int *) malloc(size * sizeof(int));
+	for (i=0; i < size; i++) img[i] = 0;
+	for(i=1; i < rows-1; i++)
+		for(j=1; j < cols-1; j++) {
+			// img[i*cols+j] = eign[i*cols+j] > 50 ? 255 : 0;
+			img[i*cols+j] = is_local_maxima(eign, cols, i, j) ? 255 : 0;
+			// printf("%0.2f\t", img[i]);
+	}
+	return img;
 }
 
 int main()
@@ -129,14 +265,36 @@ int main()
 	double *Ix = Convolute_n(flt_img, dimx, dimy, sobel_h1, 3, 1);
 	double *Iy = Convolute_n(flt_img, dimx, dimy, sobel_h2, 3, 1);
 
+	normalize_and_print(Ix, dimx, dimy, type, "boat-grad-Ix.pgm");
+	normalize_and_print(Iy, dimx, dimy, type, "boat-grad-Iy.pgm");
+
 	double *Ix2, *Iy2, *IxIy;
 
 	compute_Ix2_Iy2_IxIy(Ix, Iy, dimx, dimy, &Ix2, &Iy2, &IxIy);
 
+	double *Ix2_s = Convolute_n(Ix2, dimx, dimy, B22, 3, 2);
+	double *Iy2_s = Convolute_n(Iy2, dimx, dimy, B22, 3, 2);
+	double *IxIy_s = Convolute_n(IxIy, dimx, dimy, B22, 3, 2);
 
-	double *Ix2_s = Convolute_n(Ix2, dimx, dimy, B22, 3, 1);
-	double *Iy2_s = Convolute_n(Iy2, dimx, dimy, B22, 3, 1);
-	double *IxIy_s = Convolute_n(IxIy, dimx, dimy, B22, 3, 1);
+	double a;
+	for (a = 0.01; a < 0.2; a += 0.02) {
+		double* img = harris(dimx, dimy, Ix2_s, Iy2_s, IxIy_s, a);
+		char name[32];
+		sprintf(name, "boat-harris-%.2f.pgm", a);
+		// normalize_and_print(img, dimx, dimy, type, name);
+
+		sprintf(name, "boat-harris-norm-%.2f.pgm", a);
+		// int* norm_img = norm_harris(img, dimx, dimy);
+		int* norm_img = norm_harris_x(img, flt_img, dimx, dimy, 100);
+		// writePixmap(norm_img, dimx, dimy, type, name);
+		writePixmap(norm_img, dimx, dimy, 6, name);
+		
+		free (img);
+		free (norm_img);
+	}
+
+	int* shi2 = ShiTomasi(dimx, dimy, Ix2_s, Iy2_s, IxIy_s);
+	writePixmap(shi2, dimx, dimy, type, "boat-shi-tomasi-2.pgm");
 
 	free (image);
 	free (flt_img);
